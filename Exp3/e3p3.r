ecfe2ae87d7f5b596e038da76039a00abccdf075
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2025-05-18 16:15:17 +0530

    Create e3p3.r

---
+++ Exp3/e3p3.r
@@ -0,0 +1,18 @@
+#3) For the above mentioned data frame ,display the average marks of students of sem 5
+data <- data.frame(
+Rollno = c(1, 2, 3, 4, 5),
+Sname = c("Jack", "Jacob", "Joan", "Jasmine", "Justin"),
+dept = c("Computer Engineering", "IT", "Computer Engineering", "IT", "Computer Engineering"),
+Sem = c(3, 5, 5, 4, 3),
+Subject1_IT_marks = c(78, 90, 85, 82, 88),
+Subject2_IT_marks = c(77, 75, 80, 85, 79),
+Subject3_IT_marks = c(88, 91, 79, 83, 82),
+Subject1_Semester_Theory_marks = c(80, 85, 77,75, 79),
+Subject2_Semester_Theory_marks = c(70, 75, 73, 76, 80),
+Subject3_Semester_Theory_marks = c(80, 90, 83,85, 86),
+Attendance = c(90, 85, 92, 88, 91),
+Grade = c("A", "B", "A", "B", "A")
+)
+sem_5_avg_marks <- mean(data$Subject1_IT_marks[data$Sem == 5] +
+data$Subject2_IT_marks[data$Sem == 5] + data$Subject3_IT_marks[data$Sem == 5])
+print(sem_5_avg_marks)
