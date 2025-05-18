c582030db4d2091387b6486b1a1227caf44f0851
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2025-05-18 16:07:30 +0530

    Create e2p1.r

---
+++ Exp2/e2p1.r
@@ -0,0 +1,8 @@
+#p1 To create data frame of marks
+student_marks = data.frame( # Create data frame
+RollNo = c(1, 2, 3, 4, 5),
+Sub1 = c(85, 78, 92, 67, 80),
+Sub2 = c(75, 82, 88, 70, 79),
+Sub3 = c(90, 85, 95, 72, 88)
+)
+print(student_marks) # Print the data frame
