b0cdbf2172d3c1ca3565d21f76f94a45796dc72d
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2025-05-18 19:46:26 +0530

    Create e6p6.r

---
+++ Exp6/e6p6.r
@@ -0,0 +1,10 @@
+#p6
+library(dplyr)
+it1 <- c(17,17,16,18)
+it2 <- c(12,16,16,18)
+attendance <- c(40,75,78,88)
+marks_summary1 <- data.frame(it1,it2,attendance)
+z=mutate(marks_summary1,avg=(it1+it2)/2)
+y=transmute(marks_summary1,avg=(it1+it2)/2)
+print(z)
+print(y)
