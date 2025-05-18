dd3bba4d2462941b3eebc245eaf4af919804cac8
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2025-05-18 19:42:47 +0530

    Create e5p3.r

---
+++ Exp5/e5p3.r
@@ -0,0 +1,7 @@
+#2. write a program to implement a scatter plot
+data("iris") 
+plot(iris$Sepal.Length,iris$Sepal.Width, 
+main ="Scatterplot Example", 
+xlab ="sepal length", 
+ylab ="sepal width", 
+col="red") 
