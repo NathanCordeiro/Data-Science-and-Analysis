de8e415ed6d2b00d7537d64852387f1b6e9ef1cc
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2025-05-18 19:45:17 +0530

    Create e6p2.r

---
+++ Exp6/e6p2.r
@@ -0,0 +1,8 @@
+#p2
+library(dplyr)
+it1 <- c(17,17,16,18)
+it2 <- c(12,16,16,18)
+attendance <- c(40,75,78,88)
+marks_summary1 <- data.frame(it1,it2,attendance)
+z=distinct(marks_summary1, it1, .keep_all = TRUE)
+print(z)
